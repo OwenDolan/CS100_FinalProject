51eb7223c4e21233b3c0b9da5fdf19521253944b
SRaionido <sleon027@ucr.edu>
2022-02-15 23:12:54 -0800

    Added scene.h

---
+++ scene.h
@@ -0,0 +1,31 @@
+#ifndef SCENE_H
+#define SCENE_H
+#include <iostream>
+
+using namespace std;
+
+class Scene {
+
+private:
+    Scene* farLeft;
+    Scene* midLeft;
+    Scene* midRight;
+    Scene* farRight;
+    string desrip;
+
+public:
+    Scene(string);
+    ~Scene();
+    Scene* getFarLeft();
+    Scene* getMidLeft();
+    Scene* getMidRight();
+    Scene* getFarRight();
+    string getDescrip();
+    void setDescrip(string);
+    void setFarLeft(Scene*);
+    void setMidLeft(Scene*);
+    void setMidRight(Scene*);
+    void setFarRight(Scene*);
+};
+
+#endif
