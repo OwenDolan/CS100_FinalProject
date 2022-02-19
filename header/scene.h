ee4de130a40cd0046db1f11b5649e18e911b1f17
SRaionido <sleon027@ucr.edu>
2022-02-19 14:25:57 -0800

    Moved scene.h

--- header/scene.h
+++ header/scene.h
@@ -1,1 +1,34 @@
+#ifndef SCENE_H
+#define SCENE_H
+#include <iostream>
 
+using namespace std;
+
+class Scene {
+
+ private:
+    Scene* farLeft;
+    Scene* midLeft;
+    Scene* midRight;
+    Scene* farRight;
+    string descrip;
+    string introStory;
+
+ public:
+    Scene(string);
+    ~Scene();
+    Scene* getFarLeft();
+    Scene* getMidLeft();
+    Scene* getMidRight();
+    Scene* getFarRight();
+    string getDescrip();
+    void setDescrip(string);
+    void setFarLeft(Scene*);
+    void setMidLeft(Scene*);
+    void setMidRight(Scene*);
+    void setFarRight(Scene*);
+    virtual void outputDescrip() = 0;
+    virtual void outputIntroStory() = 0;
+};
+
+#endif
