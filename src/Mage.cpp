dbbe4a3f4cada8b4111bcebcd918f6d70ad34693
jkoh006 <jkoh006@ucr.edu>
2022-02-23 16:03:11 -0800

    moved files

---
+++ src/Mage.cpp
@@ -0,0 +1,19 @@
+#include "Mage.h"
+
+using namespace std;
+
+void Mage::setMageMP(int value) {
+	this->setMP(value);
+}
+
+void Mage::fireBlast() {
+	//..	
+}
+
+void Mage::icyWind() {
+	//..	
+}
+
+void Mage::lightningRose() {
+	//..	
+}
