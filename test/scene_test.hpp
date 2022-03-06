54b4d432cc0a3067c6801a1937aba9959b7772d8
SRaionido <sleon027@ucr.edu>
2022-03-05 21:36:21 -0800

    Added first scene test

---
+++ test/scene_test.hpp
@@ -0,0 +1,18 @@
+#ifndef __SCENE_TEST_HPP__
+#define __SCENE_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "../header/Scene/scene.h"
+#include "../header/Scene/eventscene.h"
+#include "../header/Items/Item.h"
+
+TEST(SceneTest, TestConstructor) {
+    vector<Item*> i;
+    Scene* scene = new EventScene("You are here", "Welcome", i);
+    EXPECT_EQ(scene->getLoc(), 0);
+    EXPECT_EQ(scene->getDescrip(), "You are here");
+    EXPECT_EQ(scene->getIntroStory(), "Welcome");
+}
+
+#endif
--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 
+#include "scene_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
