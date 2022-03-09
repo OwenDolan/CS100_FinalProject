ce61176e48e3dff8ca13a8ed9e548b2eb7cd6cfc
markie-bit <MaelleL060@gmail.com>
2022-03-08 17:52:25 -0800

    Added the ShopMenu class

---
+++ src/Menu/Shop_Menu_test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "../../header/Menu/Menu.hpp"
+#include "../../header/Menu/Shop_Menu.h"
+#include "../../header/Characters/Character.h"
+#include "../../header/Characters/Farmer.h"
+#include "../../header/Items/Item.h"
+#include "../../header/Items/Healing.h"
+#include "../../header/Items/Weapon.h"
+#include "../../header/Items/Armor.h"
+
+using namespace std;
+
+int main() {
+
+    Character* player = new Farmer();
+    player->setCurrency(1000);
+    ShopMenu shop = ShopMenu(player);
+    shop.print();
+
+    return 0;
+}
